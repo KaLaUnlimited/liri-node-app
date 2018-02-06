0751ff1a192461832e89e16dadc1bec84b74f5d9
KaLaUnlimited <KLasode@gmail.com>
2018-02-05 20:34:03 -0500

    files are linked

--- liri.js
+++ liri.js
@@ -1,5 +1,47 @@
-var dotenv = require("dotenv").config();
-var stuffINeed = require("keys.js");
+ var dotenv = require("dotenv").config();
+var keys = require("./keys.js");
 
-  var spotify = new Spotify(keys.spotify);
-  var client = new Twitter(keys.twitter);
+// var Spotify = require('node-spotify-api');
+//  var Twitter = require('twitter');
+
+
+ // var spotify = new Spotify(keys.spotify);
+ //  var client = new Twitter(keys.twitter);
+  console.log(keys)
+
+
+
+
+
+ 
+// var spotify = new Spotify({
+//   id: <your spotify client id>,
+//   secret: <your spotify client secret>
+// });
+ 
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+ 
+// console.log(data); 
+// });
+
+
+
+
+
+
+// var client = new Twitter({
+//   consumer_key: '',
+//   consumer_secret: '',
+//   access_token_key: '',
+//   access_token_secret: ''
+// });
+ 
+// var params = {screen_name: 'nodejs'};
+// client.get('statuses/user_timeline', params, function(error, tweets, response) {
+//   if (!error) {
+//     console.log(tweets);
+//   }
+// });
