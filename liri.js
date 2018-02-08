03375ccf7a940fe11eb2fb9efd690f5592c459fe
KaLaUnlimited <KLasode@gmail.com>
2018-02-07 23:34:11 -0500

    liri do-what-it-says function is complete

--- liri.js
+++ liri.js
@@ -14,33 +14,33 @@
 
 
 
+ function selection(choice) {
 
+     switch (choice) {
 
- switch (choice) {
+         case "my-tweets":
 
-     case "my-tweets":
-      
-         retrieveTweets();
-         break;
+             retrieveTweets();
+             break;
 
-     case "spotify-this-song":
+         case "spotify-this-song":
          
-         retrieveSpotify(query);
-         break;
+             retrieveSpotify(query);
+             break;
 
-     case "movie-this":
-        if(!query){
-         	query="Mr.Nobody";
-         }
-         retrieveMovie(query);
-         break;
+         case "movie-this":
+             if (!query) {
+                 query = "Mr.Nobody";
+             }
+             retrieveMovie(query);
+             break;
 
-     case "do-what-it-says":
-         
-         liriSays(query);
-         break;
+         case "do-what-it-says":
+
+             liriSays(query);
+             break;
+     }
  }
-
 
  function retrieveTweets() {
 
@@ -63,104 +63,98 @@
      });
  }
 
- function retrieveSpotify(a){
+ function retrieveSpotify(a) {
 
- 
-  var spotify = new Spotify(keys.spotify);
- spotify.search({ type: 'track', query: a }, function(err, data) {
-   if (err) {
-     return console.log('Error occurred: ' + err);
-   }
-  
 
-  		console.log("");
-  		console.log("");
-  		console.log("******Result*******");
+     var spotify = new Spotify(keys.spotify);
+     spotify.search({ type: 'track', query: a }, function(err, data) {
+         if (err) {
+             return console.log('Error occurred: ' + err);
+         }
 
-		console.log("Artist: " + data.tracks.items[0].artists[0].name);
-		console.log("Song: " + data.tracks.items[0].name);
-		if(data.tracks.items[0].preview_url=="null"){
-			console.log("Preview: not available ");
-		}
-		console.log("Preview: " + data.tracks.items[0].preview_url);
-		console.log( "Album: " + data.tracks.items[0].album.name);
-                 
-  
-  
- });
+
+        
+         console.log("");
+         console.log("******Result*******");
+
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+         console.log("Song: " + data.tracks.items[0].name);
+         if (data.tracks.items[0].preview_url == "null") {
+             console.log("Preview: not available ");
+         }
+         console.log("Preview: " + data.tracks.items[0].preview_url);
+         console.log("Album: " + data.tracks.items[0].album.name);
+
+
+
+     });
 
 
  }
 
- function retrieveMovie(a ){
+ function retrieveMovie(a) {
 
 
- // Then run a request to the OMDB API with the movie specified
- request("http://www.omdbapi.com/?t="+a+"&y=&plot=short&apikey=trilogy", function(error, response, body) {
+     // Then run a request to the OMDB API with the movie specified
+     request("http://www.omdbapi.com/?t=" + a + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
 
-   // If the request is successful (i.e. if the response status code is 200)
-   if (!error && response.statusCode === 200) {
+         // If the request is successful (i.e. if the response status code is 200)
+         if (!error && response.statusCode === 200) {
 
-     // Parse the body of the site and recover just the imdbRating
-     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-     //console.log("\nTitle: " + body);
-    
-     console.log("\nResult:\n\nTitle: "+ JSON.parse(body).Title
-     			+"\nYear: " + JSON.parse(body).Year
-     			+"\nIMDB rating is: " + JSON.parse(body).imdbRating  
-   	            +"\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value
-                +"\nCountry where the movie was produced: " + JSON.parse(body).Country
-     			+"\nLanguage of the movie:" + JSON.parse(body).Language
-    			+"\nPlot of the movie: " + JSON.parse(body).Plot
-     			+"\nActors: " + JSON.parse(body).Actors);
-   
-   }
- });
+             // Parse the body of the site and recover just the imdbRating
+             // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+             //console.log("\nTitle: " + body);
 
-}
+             console.log("\nResult:\n\nTitle: " + JSON.parse(body).Title +
+                 "\nYear: " + JSON.parse(body).Year +
+                 "\nIMDB rating is: " + JSON.parse(body).imdbRating +
+                 "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value +
+                 "\nCountry where the movie was produced: " + JSON.parse(body).Country +
+                 "\nLanguage of the movie:" + JSON.parse(body).Language +
+                 "\nPlot of the movie: " + JSON.parse(body).Plot +
+                 "\nActors: " + JSON.parse(body).Actors);
 
- 
- // 
+         }
+     });
 
- //  function liriSays(){
- //  	console.log("This function works now read from random.txt");
+ }
 
 
 
 
- // // This block of code will read from the "movies.txt" file.
- // // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
- // // The code will store the contents of the reading inside the variable "data"
- // fs.readFile("random.txt", "utf8", function(error, data) {
+ function liriSays() {
 
- //   // If the code experiences any errors it will log the error to the console.
- //   if (error) {
+     // This block of code will read from the "random.txt" file.
+     // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+     // The code will store the contents of the reading inside the variable "data"
+     fs.readFile("random.txt", "utf8", function(error, data) {
 
-
- //     fs.writeFile("log.txt", error, function(err){
- //         console.log(" rejoices")
+         // If the code experiences any errors it will log the error to the console.
+         if (error) {
 
 
- //     })
- //      return console.log(error);
- //   }
+             fs.writeFile("log.txt", error, function(err) {
+                 
+             })
+             return console.log(error);
+         }
 
- //   // We will then print the contents of data
- //   console.log("Spotify sequence for: " + data + " will begin...");
+         // We will then print the contents of data
+         console.log("Spotify sequence for: '" + data + "' will begin...");
+         query =data;
+         
+         selection("spotify-this-song");
+
+     });
+ }
 
 
- //     fs.writeFile("random.txt", error, function(err){
- //         console.log(" rejoices")
+ function log(data) {
+     fs.writeFile("log.txt", error, function(err) {
+         console.log(" ")
 
 
- //     })
+     })
+ }
 
- //   // // Then split it by commas (to make it more readable)
- //   // var dataArr = data.split(",");
-
- //   // // We will then re-display the content as an array for later use.
- //   // console.log(dataArr);
-
- // });
-
- //  }
+ selection(choice);
